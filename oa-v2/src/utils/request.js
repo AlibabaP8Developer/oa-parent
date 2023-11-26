0e2c7fab8666cbcc0a69d93d4771dde7ff2c0d28
Edwin <1135365441@qq.com>
2023-11-26 19:26:00 +0800

    角色管理、路由配置

--- oa-v2/src/utils/request.js
+++ oa-v2/src/utils/request.js
@@ -32,21 +32,12 @@ service.interceptors.request.use(
 
 // response interceptor
 service.interceptors.response.use(
-  /**
-   * If you want to get http information such as headers or status
-   * Please return  response => response
-  */
 
-  /**
-   * Determine the request status by custom code
-   * Here is just an example
-   * You can also judge the status by HTTP Status Code
-   */
   response => {
     const res = response.data
 
     // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
+    if (res.code !== 200) {
       Message({
         message: res.message || 'Error',
         type: 'error',
