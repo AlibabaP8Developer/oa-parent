be839de1c3fe15a32a7f5344be369e4d1952a329
Edwin <1135365441@qq.com>
2023-12-03 15:51:11 +0800

    OA云尚办公系统-spring security权限

--- service-oa/src/main/java/com/atguigu/auth/service/impl/UserDetailsServiceImpl.java
+++ service-oa/src/main/java/com/atguigu/auth/service/impl/UserDetailsServiceImpl.java
@@ -1,21 +1,27 @@
 package com.atguigu.auth.service.impl;
 
+import com.atguigu.auth.service.SysMenuService;
 import com.atguigu.auth.service.SysUserService;
 import com.atguigu.model.system.SysUser;
 import com.atguigu.security.custom.CustomUser;
+import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
+import java.util.List;
 
 @Component
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private SysUserService sysUserService;
+
+    @Autowired
+    private SysMenuService sysMenuService;
  
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -27,6 +33,15 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         if(sysUser.getStatus().intValue() == 0) {
             throw new RuntimeException("账号已停用");
         }
-        return new CustomUser(sysUser, Collections.emptyList());
+
+        // 根据userId查询用户操作权限数据
+        List<String> userPermsList= sysMenuService.findUserPermsByUserId(sysUser.getId());
+        // 封装权限数据
+        List<SimpleGrantedAuthority> authList = Lists.newArrayList();
+
+        for (String perms : userPermsList) {
+            authList.add(new SimpleGrantedAuthority(perms.trim()));
+        }
+        return new CustomUser(sysUser, authList);
     }
 }
